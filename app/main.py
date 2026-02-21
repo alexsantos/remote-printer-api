871a8131221737b2893c5d2bd99e27ab545167e1
Alexandre Santos <ajvsms@gmail.com>
2026-02-21 19:34:22 +0000

    Fix pydantic response

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 import asyncio
 import os
 import uvicorn
-from fastapi import FastAPI, HTTPException, Header, Depends
+from fastapi import FastAPI, HTTPException, Header, Depends, Request, status
+from fastapi.responses import JSONResponse
+from fastapi.exceptions import RequestValidationError
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 from typing import Annotated
 
 app = FastAPI(title="Remote Printer Cloud Gateway")
@@ -19,10 +21,28 @@ app.add_middleware(
 # Load the API Key from environment variables for security
 API_KEY_SECRET = os.getenv("PRINTER_API_KEY")
 
+
 class PrintRequest(BaseModel):
     data: str
     ip: str
     port: int = 9100
+
+    # No extra fields are permitted.
+    model_config = ConfigDict(extra='forbid')
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    # Never send to the client the details of the pydantic error.
+
+    return JSONResponse(
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        content={
+            "detail": "The payload is invalid.",
+            "type": "validation_error"
+        },
+    )
+
 
 # Dependency function to validate the API Key
 async def verify_api_key(x_api_key: Annotated[str | None, Header()] = None):
@@ -32,6 +52,7 @@ async def verify_api_key(x_api_key: Annotated[str | None, Header()] = None):
             detail="Forbidden: Invalid or missing API Key."
         )
     return x_api_key
+
 
 @app.post("/print")
 async def send_to_printer(job: PrintRequest, auth: str = Depends(verify_api_key)):
@@ -66,6 +87,7 @@ async def send_to_printer(job: PrintRequest, auth: str = Depends(verify_api_key)
             status_code=500,
             detail=f"Network Error: {str(e)}"
         )
+
 
 # Entry point for Cloud Run / Local execution
 if __name__ == "__main__":
